a106188be7b490905b7cdc2f2ed83cc83fe6118c
Ümit Yildirim <hopes61@icloud.com>
2026-07-28 09:32:26 +0200

    refactor(model): MyConnection auf zustandslose Verbindungen umstellen
    Bisher hielt MyConnection eine offene Connection und ein ResultSet als
    Felder - die Aufrufer mussten selbst ans Schließen denken, was an mehreren
    Stellen vergessen wurde. Jetzt öffnet jede Methode ihre eigene Verbindung
    per try-with-resources und schließt sie garantiert wieder.
    executeUpdate() liefert die Anzahl betroffener Zeilen zurück, sodass die
    Modelle Erfolg und Misserfolg unterscheiden können. queryList() nimmt einen
    RowMapper entgegen (funktionales Interface) und gibt fertige Objektlisten
    zurück - das ResultSet verlässt die Model-Schicht nicht mehr.

--- src/model/MyConnection.java
+++ src/model/MyConnection.java
@@ -5,63 +5,77 @@ import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+
 /**
- * @author      Ümit Yildirim <hopes61@icloud.com>
- * @copyright   Copyright (c) 2024-2026 Ümit Yildirim. Alle Rechte vorbehalten.
- * @license     Diese Datei darf nicht ohne Zustimmung des Autors weitergegeben oder verändert werden.
+ * @author Ümit Yildirim <hopes61@icloud.com>
+ * @copyright Copyright (c) 2024-2026 Ümit Yildirim. Alle Rechte vorbehalten.
+ * @license Diese Datei darf nicht ohne Zustimmung des Autors weitergegeben oder verändert werden.
  */
 public class MyConnection
 {
-	private String url, username, password, database;
-	private Connection connector;
-	private Statement statement;
-	
-	public MyConnection(String url, String username, String password)
-	{
-		this.url = url;
-		this.username = username;
-		this.password = password;
-		
-		try
-		{
-			connector = DriverManager.getConnection(this.url, this.username, this.password);
-			this.statement = this.connector.createStatement();
+    private String url, username, password;
 
-		}
-		catch (SQLException e)
-		{
-			System.out.println(e.getMessage());
-		}
-	}
+    public MyConnection(String url, String username, String password)
+    {
+        this.url = url;
+        this.username = username;
+        this.password = password;
+    }
 
+    public Connection getConnection()
+    {
+        Connection connector = null;
+        try
+        {
+            connector = DriverManager.getConnection(url,username,password);
+        }
+        catch (SQLException e)
+        {
+            System.out.println("Es konnte keine Verbindung hergestellt werden."+e.getMessage());
+        }
+        return connector;
+    }
 
-	public void execute(String query)
-	{
-		try
-		{
-			this.statement.execute("" + query);
-		}
-		catch (SQLException e)
-		{
-			System.out.println("Oh NO Wrong Query"+e);
-		}
-	}
+    public int executeUpdate(String query)
+    {
+        int retVal =0;
+        try (Connection con = getConnection();
+             Statement st = con.createStatement())
+        {
+            retVal = st.executeUpdate(query); // Anzahl betroffener Zeile
+        }
+        catch (SQLException e)
+        {
+            // Technische Ursache ausgeben. Fachlich deuten koennen nur die Modelle:
+            // ein doppeltes Kennzeichen meldet Fahrzeug als "Vorhanden" an die View.
+            System.out.println("Update fehlgeschlagen: " + e.getMessage());
+        }
+        return retVal;
+    }
 
-	public ResultSet executeQuery(String query)
-	{
-		ResultSet rs = null;
-		
-		try
-		{
-			rs = this.statement.executeQuery(query);
-		}
-		catch (SQLException e)
-		{
-			System.out.println("Wrong query, try again!"+e);
-		}
-			
-		return rs;
-	}
+    public <T> List<T> queryList(String query,RowMapper<T> mapper)
+    {
+        List<T> retVal = new ArrayList<>();
+        try(
+            Connection con= getConnection();
+            Statement st = con.createStatement();
+            ResultSet rs = st.executeQuery(query)
+           )
+        {
+            while(rs.next())
+            {
+                retVal.add(mapper.mapRow(rs));
+            }
+        }
+        catch (SQLException e)
+        {
+            System.out.println(e);
+        }
+        return retVal;
+    }
+
 }
 
 
