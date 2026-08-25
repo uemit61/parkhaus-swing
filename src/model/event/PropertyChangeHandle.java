b976ec9b7ace023b88e3e1d6e93f3ea616b9bb07
Ümit Yildirim <hopes61@icloud.com>
2026-08-25 04:45:07 +0200

    refactor(model): PropertyChangeHandle nach model.event verschieben
    Der Ereignis-Kanal ist weder Datenzugriff noch Fachlogik, sondern der
    Draht zur View - und bekommt deshalb ein eigenes Paket.

---
+++ src/model/event/PropertyChangeHandle.java
@@ -0,0 +1,47 @@
+package model.event;
+
+import java.beans.PropertyChangeListener;
+import java.beans.PropertyChangeSupport;
+
+/**
+ * Gemeinsamer Ereignis-Kanal zwischen Model und View. Alle Modelle melden über
+ * dieselbe Instanz, die Views registrieren sich auf die Namen der Ereignisse,
+ * die sie interessieren.
+ *
+ * @author      Ümit Yildirim <hopes61@icloud.com>
+ * @copyright   Copyright (c) 2024-2026 Ümit Yildirim. Alle Rechte vorbehalten.
+ * @license     Diese Datei darf nicht ohne Zustimmung des Autors weitergegeben oder verändert werden.
+ */
+public class PropertyChangeHandle
+{
+    private final PropertyChangeSupport support;
+
+    public PropertyChangeHandle(PropertyChangeSupport support)
+    {
+        this.support = support;
+    }
+
+    /**
+     * Meldet eine View für ein bestimmtes Ereignis an.
+     *
+     * @param probName Name des Ereignisses, zum Beispiel "Alarm"
+     * @param pcl      die View, die darauf reagieren soll
+     */
+    public void addPropertyChangeListener(String probName, PropertyChangeListener pcl)
+    {
+        support.addPropertyChangeListener(probName, pcl);
+    }
+
+    /**
+     * Meldet ein Ereignis an alle Views, die sich auf diesen Namen angemeldet haben.
+     *
+     * @param propName Name des Ereignisses
+     * @param newValue mitgeschickte Daten, oder {@code null}, wenn das Ereignis für
+     *                 sich allein spricht
+     */
+    public void propertyChange(String propName, Object newValue)
+    {
+        support.firePropertyChange(propName, null, newValue);
+    }
+
+}
